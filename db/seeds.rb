3ecb05f6ebf916c9279ef7509b0e4a982b8613e7
nazzzar502 <NTT3G4X@POSTEITALIANE.IT>
2023-01-23 13:25:44 +0200

    Add stores index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
 
 Product.create!(title: 'Build Chatbot Interactions',
   description:
@@ -20,5 +21,19 @@ Product.create!(title: 'Build Chatbot Interactions',
   can participate. Exercise your creativity by creating chatbot
   skills for communicating, information, and fun.
   </p>},
-  image_url: 'dpchat.jpg',
+  image_url: 'ruby.jpg',
   price: 20.00)
+
+  Product.create!(title: 'Duis adipisicing esse exercitation incididunt pariatur.',
+    description:
+    %{<p>
+    Nostrud non Lorem veniam et amet.Sint amet in elit sit ad cillum minim nostrud aliqua irure et laboris qui quis.
+    Elit cillum commodo commodo enim enim sit veniam sit.Consequat reprehenderit incididunt est Lorem.
+    Aute laboris nisi labore est quis deserunt aute irure in amet commodo.Sit duis cupidatat incididunt incididunt ad culpa incididunt ullamco Lorem deserunt officia cupidatat laboris.
+    Excepteur non mollit sunt consectetur exercitation deserunt in incididunt id anim nulla culpa.Quis velit ut minim culpa amet.
+    Magna consequat eu cillum duis incididunt amet ex nostrud.Ipsum ut culpa enim ipsum ullamco eiusmod non ut sit aute sunt adipisicing tempor consequat.
+    Aliqua occaecat mollit tempor pariatur et duis ut.Ullamco do nulla ad adipisicing eiusmod.
+    Minim ut occaecat duis proident officia sunt exercitation.
+    </p>},
+    image_url: 'placeholder.jpg',
+    price: 20.00)
