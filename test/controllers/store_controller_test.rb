b7854dc61ca9178dc2e942da8e5ac7d6a38b845b
nazzzar502 <NTT3G4X@POSTEITALIANE.IT>
2023-01-23 13:51:50 +0200

    Add functional test

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'section.section', minimum: 3
+    assert_select 'h4.title', minimum: 3
+    assert_select 'h6.subtitle', minimum: 3
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
