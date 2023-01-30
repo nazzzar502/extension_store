9d5db17e0883ed7a14e8a51586d03aef3e6dcfd3
nazzzar502 <NTT3G4X@POSTEITALIANE.IT>
2023-01-30 14:21:42 +0200

    Connect line_items to orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,24 @@
 class Order < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+
   enum pay_type: {
     'Check' => 0,
     'Credit card' => 1,
     'Purchase order' => 2,
   }
+
+  validates :name, :email, :address, :pay_type, presence: true
+  validates :pay_type, inclusion: pay_types.keys
+  validates :email, allow_blank: false, format: {
+    with: /.+@.+/,
+    message: 'Please enter a valid email address.'
+  }
+
+ 
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
 end
